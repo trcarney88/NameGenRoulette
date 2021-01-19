ea3ab81abfd62ee74d4b0c59c2d7e119e1e17b38
Todd Carney <toddcarney44@gmail.com>
2021-01-19 15:03:47 -0700

    Change API from birds to random dictionay words

---
+++ get_domain_name.py
@@ -0,0 +1,27 @@
+import os
+import time
+import requests
+
+def get_random_word():
+    url = "https://wordsapiv1.p.rapidapi.com/words/"
+
+    querystring = {
+        "random": "true",
+        "letters": "8",
+        "letterPattern": "^[A-z]+$"
+    }
+
+    headers = {
+        # Get your API key at https://rapidapi.com/dpventures/api/wordsapi/pricing
+        'x-rapidapi-key': os.environ['WORD_API'],
+        'x-rapidapi-host': "wordsapiv1.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    #print(response.text)
+    return response.json()['word']
+
+if __name__ == '__main__':
+
+    print(get_random_word())
